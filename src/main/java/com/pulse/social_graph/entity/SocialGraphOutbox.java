e3b38756dbd45547e2362a382b720a8f1aeef91c
wlsdks <wlsdks12@naver.com>
2024-07-07 13:08:47 +0900

    [PULS-3] :sparkles: 아웃박스 엔티티 추가

---
+++ src/main/java/com/pulse/social_graph/entity/SocialGraphOutbox.java
@@ -0,0 +1,61 @@
+package com.pulse.social_graph.entity;
+
+import com.pulse.social_graph.entity.constant.MessageStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * 이벤트 발행 및 Kafka 메시지 송/수신을 관리하기 위한 Outbox 엔티티
+ */
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "content_outbox")
+public class SocialGraphOutbox extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "event_type", nullable = false)
+    private String eventType;   // 토픽정보 ex.MemberCreatedEvent
+
+    @Column(name = "event_id", nullable = false)
+    private Long eventId;       // 이벤트 내부의 id 필드를 저장. ex) memberId: 1L
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "message_status", nullable = false)
+    private MessageStatus status;      // Kafka 메시지 처리 상태 (예: PENDING, PROCESSED, SUCCESS, FAIL)
+
+    @Column(name = "processed_at")
+    private LocalDateTime processedAt; // Kafka 메시지 처리 시간 (처리된 경우)
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SocialGraphOutbox that)) return false;
+        return id != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    public void changeStatus(MessageStatus messageStatus) {
+        this.status = messageStatus;
+    }
+
+    public void changeProcessedAt(LocalDateTime now) {
+        this.processedAt = now;
+    }
+
+}

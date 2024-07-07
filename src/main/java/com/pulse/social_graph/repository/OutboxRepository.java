60742585590305a054368cf633c2a1bf6afc0c5e
wlsdks <wlsdks12@naver.com>
2024-07-07 13:10:54 +0900

    [PULS-5] :sparkles: 이벤트 라이브러리 인터페이스 구현

---
+++ src/main/java/com/pulse/social_graph/repository/OutboxRepository.java
@@ -0,0 +1,12 @@
+package com.pulse.social_graph.repository;
+
+import com.pulse.social_graph.entity.SocialGraphOutbox;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OutboxRepository extends JpaRepository<SocialGraphOutbox, Long> {
+
+    Optional<SocialGraphOutbox> findByEventIdAndEventType(Long id, String eventType);
+
+}
